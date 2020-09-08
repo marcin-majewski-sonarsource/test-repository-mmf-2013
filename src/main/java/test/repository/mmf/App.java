771562f1c22a4fe0c132e8cc6b39fe700b4ec117
Marcin Majewski <marcin.majewski@sonarsource.com>
2020-09-08 11:54:54 +0200

    * PR commit another

--- src/main/java/test/repository/mmf/App.java
+++ src/main/java/test/repository/mmf/App.java
@@ -5,7 +5,7 @@ package test.repository.mmf;
 
 public class App {
     public String getGreeting() {
-        String speech = "Now is the time for all good people to come to the aid of their country.";
+        String speech = "Now is the time for all good people to come to the aid of their country Change.";
         String substr1 = speech.substring(-1, speech.length());  // Noncompliant; start and end values both bad
         String substr2 = speech.substring(speech.length(), 0); // Noncompliant, start value must be < end value
         char ch = speech.charAt(speech.length());  // Noncompliant
